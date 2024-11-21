848f5281b43164eb61f2a5fdff897d4b0d3d1ecc
TejasChaudhari <tejaschaudhari2541@gmail.com>
2024-11-20 22:36:39 -0800

    Create Assignment_08.cpp

---
+++ Assignment_08.cpp
@@ -0,0 +1,8 @@
+/*
+A library needs to manage a collection of books (data fields such
+as book title, author, ISBN, availability status, and other relevant
+information), maintain information about each book, support
+operations like adding, removing, and searching for books, and
+provide efficient access to library resources. Use the Concept of
+Linked List for implementation of Library Management system.
+*/
