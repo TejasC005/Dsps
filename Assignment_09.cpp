7a5d0cdb02cf9c0c71fceff81557b4ff988c1f4d
TejasChaudhari <tejaschaudhari2541@gmail.com>
2024-11-20 22:37:28 -0800

    Create Assignment_09.cpp

---
+++ Assignment_09.cpp
@@ -0,0 +1,9 @@
+/*
+A Charity Management system need to manage the information
+about donors, donations and other relevant data. System need to
+maintain information about each donors. System should able to
+add new donor, remove the donor, update the information of the
+donor, search the specific donor also it should display the
+information of all the donor which are present in the database. Use
+the concept of the Linked List to implement the above system
+*/
